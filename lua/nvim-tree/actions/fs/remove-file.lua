b360bf1afd9f546dd58a17dfcca77932b43b9406
Roger Takeshita <roger.takeshita@gmail.com>
2023-10-09 11:13:14 -0400

    feat: Should force refresh after deleting file

--- lua/nvim-tree/actions/fs/remove-file.lua
+++ lua/nvim-tree/actions/fs/remove-file.lua
@@ -124,6 +124,7 @@ function M.remove(node)
     clear_buffer(node.absolute_path)
   end
   -- notify.info(notify_node .. " was properly removed.")
+  require("nvim-tree.renderer").draw()
 end
 
 ---@param node Node
